11a6ac42be0e3827d06123b938e1e08eac5a3ece
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-12-02 16:15:32 +0700

    add cap and tauri

--- docs/_next/static/chunks/c34255f6868dff68.js
+++ docs/_next/static/chunks/c34255f6868dff68.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/d706a91fa2a0662e.js",
   "static/chunks/6061ee5d571043e2.js",
-  "static/chunks/d638f748a7550f0a.css",
-  "static/chunks/turbopack-ced7b9759ef535f0.js"
+  "static/chunks/c5b19b659391ee99.css",
+  "static/chunks/turbopack-a549fe14d0b5aa55.js"
 ])
