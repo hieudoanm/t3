c1597ebef480b74172824e99c5a37805149d5b1c
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-09 19:56:43 +0700

    update head

--- docs/_next/static/chunks/5033ccface3d60bc.js
+++ docs/_next/static/chunks/5033ccface3d60bc.js
@@ -1,7 +1,7 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/c9d0f5fb43f0fe27.js",
+  "static/chunks/ea357d5b76065825.js",
   "static/chunks/4ebc992e9cb9eb40.js",
   "static/chunks/d776695e9c756dc9.js",
   "static/chunks/488abb06783124b4.css",
-  "static/chunks/turbopack-6cb46ddb203a1def.js"
+  "static/chunks/turbopack-184f5a49f87169b1.js"
 ])
--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -8,6 +8,9 @@ const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <title>T3 - Tic-Tac-Toe</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
     </>
