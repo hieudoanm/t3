c3c9f722a2003f660b6dafd9b96526e93e79e4f5
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-15 21:49:33 +0700

    update prettier

--- packages/app/capacitor.config.ts
+++ packages/app/capacitor.config.ts
@@ -1,11 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-	appId: 'io.github.hieudoanm.t3',
-	appName: 'T3',
-	webDir: '../../docs',
-	android: { path: 'mobile/android' },
-	ios: { path: 'mobile/ios' },
+  appId: 'io.github.hieudoanm.t3',
+  appName: 'T3',
+  webDir: '../../docs',
+  android: { path: 'mobile/android' },
+  ios: { path: 'mobile/ios' },
 };
 
 export default config;
